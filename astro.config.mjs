94123984654e3d45401c3769bb04423b7b6702f1
wisftock <emaildeprueb4s@gmail.com>
2026-05-05 14:18:14 -0500

    fix: cambiar outDir a _site para evitar conflicto con carpeta public/ en CI

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ export default defineConfig({
   site: 'https://wisftock.gitlab.io/page-libreria-component',
   base: '/page-libreria-component/',
   compressHTML: true,
+  outDir: '_site',
   build: {
     assets: '_assets',
   },
